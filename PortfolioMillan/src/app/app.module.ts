8e1410962718436528928b4084a1f1c698deba8f
Nacho <ignacio.millan1986@gmail.com>
2022-08-04 02:44:12 -0300

    Header logo y redes

--- PortfolioMillan/src/app/app.module.ts
+++ PortfolioMillan/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    RedesSocialesComponent
   ],
   imports: [
     BrowserModule,
